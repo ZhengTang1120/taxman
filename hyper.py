870b94b5eb392703d77699eac2f982f490766b16
Zheng Tang <oscartang1120@gmail.com>
2019-03-28 14:50:26 -0700

    python code

---
+++ hyper.py
@@ -0,0 +1,111 @@
+import numpy as np
+import dynet_config
+dynet_config.set(
+    mem=10240,
+    random_seed=1,
+    # autobatch=True
+)
+import dynet as dy
+
+import pickle
+
+class Hyper:
+    def __init__(self, words, w2i, embeds, embeds_dim, hidden_size, k):
+        self.words = words
+        self.vocab_size = len(words)
+        self.w2i = w2i
+        self.embeds_dim = embeds_dim
+        self.hidden_size = hidden_size
+        self.embeds = embeds
+        self.k = k
+
+        self.model = dy.Model()
+        self.trainer = dy.SimpleSGDTrainer(self.model)
+
+        self.word_embeddings = self.model.add_lookup_parameters((len(self.w2i), self.embeds_dim))
+        for i, w in enumerate(self.words):
+            if w in self.embeds:
+                self.word_embeddings.init_row(i, self.embeds[w])
+        self.Phis = list()
+        for i in range(self.k):
+            self.Phis.append(self.model.add_parameters((self.embeds_dim, self.embeds_dim)))
+        self.W = self.model.add_parameters((1, 24))
+        self.b = self.model.add_parameters((1))
+
+    def save(self, name):
+        params = (
+            self.words, self.w2i, self.embeds_dim, self.embeds, self.hidden_size, self.k
+        )
+        # save model
+        self.model.save(f'{name}.model')
+        # save pickle
+        with open(f'{name}.pickle', 'wb') as f:
+            pickle.dump(params, f)
+
+    @staticmethod
+    def load(name):
+        with open(f'{name}.pickle', 'rb') as f:
+            params = pickle.load(f)
+            parser = Hyper(*params)
+            parser.model.populate(f'{name}.model')
+            return parser
+
+    def train(self, trainning_set):
+        loss_chunk = 0
+        loss_all = 0
+        total_chunk = 0
+        total_all = 0
+        losses = []
+        for datapoint in trainning_set:
+            query = datapoint[0]
+            eq = dy.average([self.word_embeddings[self.w2i[w]] if w in self.w2i else self.word_embeddings[0] for w in query])
+            hyper = datapoint[1]
+            eh = dy.average([self.word_embeddings[self.w2i[w]] if w in self.w2i else self.word_embeddings[0] for w in hyper])
+            t = dy.scalarInput(datapoint[2])
+            Ps = []
+            for i in range(self.k):
+                Ps.append(self.Phis[i].expr() * eq)
+            P = dy.transpose(dy.concatenate_cols(Ps))
+            s = P * eh
+            y = dy.reshape(dy.logistic(self.W.expr() * s + self.b.expr()), (1,))
+
+            losses.append(dy.binary_log_loss(y, t))
+
+            # process losses in chunks
+            if len(losses) > 50:
+                loss = dy.esum(losses)
+                l = loss.scalar_value()
+                loss.backward()
+                self.trainer.update()
+                dy.renew_cg()
+                losses = []
+                loss_chunk += l
+                loss_all += l
+                total_chunk += 1
+                total_all += 1
+
+        # consider any remaining losses
+        if len(losses) > 0:
+            loss = dy.esum(losses)
+            loss.scalar_value()
+            loss.backward()
+            self.trainer.update()
+            dy.renew_cg()
+        print(f'loss: {loss_all/total_all:.4f}')
+
+    def get_hypers(self, query, vocab, w2i):
+        ans = list()
+        eq = dy.average([self.word_embeddings[w2i[w]] if w in self.w2i else self.word_embeddings[0] for w in query])
+        Ps = []
+        for i in range(self.k):
+            Ps.append(self.Phis[i].expr() * eq)
+        P = dy.transpose(dy.concatenate_cols(Ps))
+        for ws in vocab:
+            eh = dy.average([self.word_embeddings[self.w2i[w]] if w in self.w2i else self.word_embeddings[0] for w in ws])
+            s = P * eh
+            y = dy.reshape(dy.logistic(self.W.expr() * s + self.b.expr()), (1,))
+            if len(ans) <=15 and y.npvalue() > 0.5:
+                ans.append(ws)
+            if len(ans)>15:
+                break
+        return ans
---
+++ train.py
@@ -0,0 +1,105 @@
+from hyper import *
+import argparse
+import random
+import pickle
+import math
+import numpy as np
+import re
+
+def sanitizeWord(w):
+    if w.startswith("$T"):
+        return w
+    if w == ("xTHEMEx"):
+        return "xTHEMEx"
+    if w == ("xTRIGGERx"):
+        return "xTRIGGERx"
+    w = w.lower()
+    if is_number(w):
+        return "xnumx"
+    w = re.sub("[^a-z_]+","",w)
+    if w:
+        return w
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
+
+def load_embeddings(file):
+    emb_matrix = None
+    emb_dict = dict()
+    for line in open(file):
+        if not len(line.split()) == 2:
+            if "\t" in line:
+                delimiter = "\t"
+            else:
+                delimiter = " "
+            line_split = line.rstrip().split(delimiter)
+            # extract word and vector
+            word = line_split[0]
+            vector = np.array([float(i) for i in line_split[1:]])
+            embedding_size = vector.shape[0]
+            emb_dict[word] = vector
+    return emb_dict
+
+if __name__ == '__main__':
+    random.seed(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pos')
+    parser.add_argument('neg')
+    args = parser.parse_args()
+    
+    words = ["UNKNOWN"]
+    w2i = {"UNKNOWN":0}
+    candidates = list()
+    for line in open("SemEval2018-Task9/vocabulary/1A.english.vocabulary.txt"):
+        temp = line.strip().lower().split(" ")
+        candidates.append(temp)
+        for t in temp:
+            t = sanitizeWord(t)
+            if t and t not in w2i:
+                w2i[t] = len(words)
+                words.append(t)
+    embeds = load_embeddings("glove.6B.200d.txt")
+    trainning_set = list()
+    for line in open(args.pos):
+        pairs = line.strip().split("\t")
+        for p in pairs:
+            p = p.lower().strip().replace("(","").replace(")","")
+            q = p.split(",")[0]
+            h = p.split(",")[1]
+            trainning_set.append((q, h, 1))
+    for line in open(args.neg):
+        pairs = line.strip().split("\t")
+        for p in pairs:
+            p = p.lower().strip().replace("(","").replace(")","")
+            q = p.split(",")[0]
+            h = p.split(",")[1]
+            trainning_set.append((q, h, 0))
+    print ("Trainning")
+    
+    model = Hyper(words, w2i, embeds, 200, 200, 24)
+    for i in range(200):
+        random.shuffle(trainning_set)
+        model.train(trainning_set)
+        if ((i+1) % 10) == 0:
+            with open("result"+str(i)+".txt", "w") as f:
+                for line in open("SemEval2018-Task9/test/data/1A.english.test.data.txt"):
+                    query = line.strip().lower().split("\t")[0]
+                    f.write(query)
+                    for c in model.get_hypers(query, candidates, w2i):
+                        c = ' '.join(c)
+                        f.write("\t"+c)
+                    f.write("\n")
+            model.save("model"+str(i))
+
+
